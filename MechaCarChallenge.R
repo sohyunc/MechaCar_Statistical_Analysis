ab1692e5469bd11a6dd0039bb4a6284ea1cc1bba
sohyunc <79174940+sohyunc@users.noreply.github.com>
2021-06-20 12:01:25 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,40 +1,31 @@
-#Deliverable 1
-#Load the dplyr package
 library(dplyr)
-
-#Read the MechaCar_mpg.csv as a dataframe
-MechaCar<-read.csv(file = 'MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
-
-#Perform the linear regression using lm()function passing all six variables with mpg as the y axis
-Mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
-
-#Use the summary() function to determine the p-value and the r-squared value and save code to GitHub
-summary(Mecha_lm)
-
-#Deliverable 2
-#Read the Suspension_Coil.csv file as a table
-Suspension<-read.csv(file = 'Suspension_coil.csv',check.names = F,stringsAsFactors = F)
-
-#Create a total_summary dataframe using the summarize() function
-#to get the mean, median, variance, and standard deviation of the PSI column
-total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
-
-#Create a lot_summary dataframe using groupby() and summarize() functions 
-#to group each lot by the mean, median, variance, and standard deviation of the PSI column
-lot_summary <-Suspension %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+library(tidyverse)
 
 
-#Deliverable 3
-#Using t.test() function to determine if the PSI across all lots is 
-#statistically different from the population mean of 1500 ppsi
-#First test all lots
-t.test(Suspension$PSI,mu=1500)
+MechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv')
+Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv')
 
-#Test for lot 1
-t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+head(MechaCar_mpg_table)
 
-#Test for Lot 2
-t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_table)
 
-#Test for Lot 3
-t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCar_mpg_table))
+
+
+total_summary <-  data.frame(
+  Mean=mean(Suspension_Coil_table$PSI),
+  Median=median(Suspension_Coil_table$PSI),
+  Variance=var(Suspension_Coil_table$PSI),
+  SD=sd(Suspension_Coil_table$PSI))
+
+show(total_summary)
+
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+t.test(x=Suspension_Coil_table$PSI,mu=1500)
+lot1_subset <- subset(Suspension_Coil_table, Manufacturing_Lot=='Lot1') 
+t.test(x=lot1_subset$PSI, mu=1500) 
+lot2_subset <- subset(Suspension_Coil_table, Manufacturing_Lot=='Lot2') 
+t.test(x=lot2_subset$PSI, mu=1500) 
+lot3_subset <- subset(Suspension_Coil_table, Manufacturing_Lot=='Lot3') 
+t.test(x=lot3_subset$PSI, mu=1500)
